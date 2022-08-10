59890be0a647e7b6b539dbc002c579a2f212ca08
Hatcattt <hatcattt.dev@gmail.com>
2022-08-10 17:21:53 +0200

    change name to username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable(); // nullable pour devoir le vérifier aussi
+            $table->string('username')->nullable(); // nullable pour devoir le vérifier aussi
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
