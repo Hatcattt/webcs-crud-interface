34aab6551d153fb881c106357be62175d88d596b
Hatcattt <hatcattt.dev@gmail.com>
2022-08-11 12:26:10 +0200

    add font-awesome package

---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Crud Project Interface for my course ( WEBCS ).">
+    <meta name="author" content="Maxence Mercier / hatcattt">
+    <title>Crud Project · WEBCS</title>
+
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pico.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/all.css') }}">
+
+    @yield('head+')
+</head>
