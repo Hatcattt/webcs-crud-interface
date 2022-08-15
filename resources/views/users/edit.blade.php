4719ec5e4c7990adccd415fb86edc007148c8811
Hatcattt <hatcattt.dev@gmail.com>
2022-08-15 11:11:47 +0200

    change small things

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app-master')
 
-@section('title', "Changement de rôle")
-@section('title_small', "Ici, vous pouvez changer le rôle des users.")
+@section('title', "Users")
+@section('title_small', "Ici, vous pouvez voir les users présents dans votre système.")
 
 @section('content')
     <figure>
@@ -12,7 +12,7 @@
                 <th scope="col">Username</th>
                 <th scope="col">Email</th>
                 <th scope="col">Rôle</th>
-                <th scope="col">Make</th>
+{{--                <th scope="col">Make</th>--}}
             </tr>
             </thead>
             <tbody>
@@ -24,23 +24,23 @@
                     <td>{{ $user->username }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->role }}</td>
-                    <td>
-                        @if ($user->role === 'reader')
-                        <label for="switch">
-                            <input type="checkbox" id="switch" name="switch" role="switch">Admin
-                        </label>
-                        @else
-                            <label for="switch">
-                                <input type="checkbox" id="switch" name="switch" role="switch">Reader
-                            </label>
-                        @endif
-                    </td>
+{{--                    <td>--}}
+{{--                        @if ($user->role === 'reader')--}}
+{{--                        <label for="switch">--}}
+{{--                            <input type="checkbox" id="switch" name="switch" role="switch">Admin--}}
+{{--                        </label>--}}
+{{--                        @else--}}
+{{--                            <label for="switch">--}}
+{{--                                <input type="checkbox" id="switch" name="switch" role="switch">Reader--}}
+{{--                            </label>--}}
+{{--                        @endif--}}
+{{--                    </td>--}}
                 </tr>
             @endforeach
             </tbody>
         </table>
         <div>
-            <a role="button" title="Non implémenté" class="outline" disabled>Save</a>
+            <a role="button" class="outline" disabled>Save</a>
         </div>
     </figure>
 @endsection
